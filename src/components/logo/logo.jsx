820e68bfc38adef5d82488afc3f40476cfb2b672
saiphanindra1010ddsd <saiphanindra1010@gmail.com>
2024-09-12 23:38:43 +0530

    fix logo

---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../../assets/Designer.png'
+import styles from './logo.module.css'
+const logo = () => {
+  return (
+<>
+<img src={Logo} alt="Logo" className={styles.img} />Algo gen.ai
+</>
+
+  )
+}
+
+export default logo
