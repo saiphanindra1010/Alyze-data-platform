0d459494b8dcd6734cfb1b87b8ed12a60ee094b9
saiphanindra1010ddsd <saiphanindra1010@gmail.com>
2024-09-25 23:33:23 +0530

    adding pkg

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import Logo from '../../assets/Designer.png'
 import styles from './logo.module.css'
 const logo = () => {
   return (
 <>
-<img src={Logo} alt="Logo" className={styles.img} />Algo gen.ai
+
+<img src={Logo} alt="Logo" className="w-9 py-8" />
+
 </>
 
   )
