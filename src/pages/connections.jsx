d846da02615fd1489ec7040d10fe7f94f2ba1948
saiphanindra1010ddsd <saiphanindra1010@gmail.com>
2024-10-03 01:19:02 +0530

    connection page

--- src/pages/connections.jsx
+++ src/pages/connections.jsx
@@ -1,41 +1,18 @@
 import Createconnection from "@/components/createconnection/createconnection"
 
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination"
+
 import Layout from "@/components/layout/layout"
 import Connectionslist from "@/pages/Connectionslist"
 const connections = () => {
   return (
     <Layout>
-    <div >
-      <div className="flex justify-end">  
+    <div>
+    <div className=" w-full max-w-7xl mx-auto h-full flex flex-col">
       <Createconnection/>
       </div>
 
 <Connectionslist/>
-<Pagination>
-  <PaginationContent>
-    <PaginationItem>
-      <PaginationPrevious href="#" />
-    </PaginationItem>
-    <PaginationItem>
-      <PaginationLink href="#">1</PaginationLink>
-    </PaginationItem>
-    <PaginationItem>
-      <PaginationEllipsis />
-    </PaginationItem>
-    <PaginationItem>
-      <PaginationNext href="#" />
-    </PaginationItem>
-  </PaginationContent>
-</Pagination>
+
 
     </div>
     </Layout>
