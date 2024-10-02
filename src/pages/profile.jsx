89fa72ed4676b443ad74bb14fb01e0ab9ba60b79
saiphanindra1010ddsd <saiphanindra1010@gmail.com>
2024-10-03 00:48:38 +0530

    profile page

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import Layout from '@/components/layout/layout'
-import { Label } from '@radix-ui/react-dropdown-menu'
-import { Input } from '@/components/ui/input'
-const profile = () => {
+import React from 'react';
+import Layout from '@/components/layout/layout';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Progress } from "@/components/ui/progress"
+
+const Profile = () => {
   return (
-    
     <Layout>
-    <div>
+      <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-50">
 
-        <Label>Name</Label>
-        <Input type="text"></Input> 
-    </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default profile
+export default Profile;
